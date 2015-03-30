b5810405cae9a3bc4cae5fc64e41d3371d681e9c
alumno05 <alumno05@TPP-LAB05-19.TPP.UPEU.EDU.PE>
2015-03-30 16:02:10 -0500

    josias

--- src/herencia/Herencia.java
+++ src/herencia/Herencia.java
@@ -15,7 +15,9 @@ public class Herencia {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         System.out.println(" Josias");
     }
     
+    
+     
 }
