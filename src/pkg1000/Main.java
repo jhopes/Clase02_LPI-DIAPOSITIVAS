9fc3ea8e37592dd7eae6c12ad371e4b86a1f4ec9
alondra971 <katerinealondra@gmail>
2015-03-30 18:12:58 -0500

    Par

---
+++ src/pkg1000/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg1000;
+
+/**
+ *
+ * @author Parisan
+ */
+public class Main {
+
+  
+    public static void main(String[] args) {
+       
+    int par=0;
+    while(par<=1000)
+    {
+        System.out.println(""+ par);
+        par=par+2;
+    }
+    }
+}
